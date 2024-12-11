622db9be228836f32e8ca121dd0644b6d73e4cc4
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-11 18:58:17 +0700

    init eureka server

---
+++ community_post_backend/src/test/java/dev/skyherobrine/backend/CommunityPostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
